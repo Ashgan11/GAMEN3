a28ecf2b6ab01c80af9f2738b40ea2711226260f
Kevin Fayard <tyball@pt.lu>
2021-10-20 13:52:28 +0200

    Initial file commit

---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLife : MonoBehaviour
+{
+    public float maxHealth = 100;
+    private float currentHealth;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth   = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value    = currentHealth;
+    }
+
+    void changeHealth(float delta)
+    {
+        if (currentHealth + delta <= maxHealth && currentHealth + delta >= 0)
+        {
+            currentHealth += delta;
+            slider.value = currentHealth;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Numpad+-"))
+        {
+            changeHealth(Input.GetAxis("Numpad+-") * 10);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float movementSpeed = 12f;
+    public float jumpForce = 15f;
+
+    public float gravity = 30f;
+
+    private Vector3 gravityVector;
+    private Vector3 move;
+
+    private void Update()
+    {
+        //Player Movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        if (controller.isGrounded) //Player cannot alter movement velocity mid-air.
+        {
+            if (z < 0)
+            {
+                move = transform.right * x * 0.75f + transform.forward * z * 0.5f;
+            }
+            else
+            {
+                move = transform.right * x * 0.75f + transform.forward * z;
+            }            
+        }
+        controller.Move(move * movementSpeed * Time.deltaTime);
+
+        //Gravity
+        if (!controller.isGrounded)
+        {
+            gravityVector.y -= gravity * Time.deltaTime;
+            controller.Move(gravityVector * Time.deltaTime);
+        }
+        else
+        {
+            gravityVector.y = -2f;
+        }
+
+        //Jumping
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            gravityVector.y = jumpForce;
+            controller.Move(gravityVector * Time.deltaTime);
+        }        
+    }
+}
